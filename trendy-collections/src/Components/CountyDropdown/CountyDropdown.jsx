512acf531da96e816d39fdde60879b9033a9769f
HassanMunene <awanzihassan@gmail.com>
2025-04-06 23:01:15 +0300

    made the county dialog popup dynamic

--- trendy-collections/src/Components/CountyDropdown/CountyDropdown.jsx
+++ trendy-collections/src/Components/CountyDropdown/CountyDropdown.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from '@mui/material/Button';
 import { FaAngleDown } from "react-icons/fa";
 import Dialog from '@mui/material/Dialog';
@@ -6,54 +6,72 @@ import { IoIosSearch } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
 import Slide from '@mui/material/Slide';
 
+import { KENYAN_COUNTIES } from "../../data/kenyan_counties";
+
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
 const CountyDropdown = () => {
     const [isOpenDialog, setIsOpenDialog] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filteredCounties, setFilteredCounties] = useState(KENYAN_COUNTIES);
+    const [selectedCounty, setSelectedCounty] = useState("");
+
+    useEffect(() => {
+        if (searchTerm.trim() === "") {
+            setFilteredCounties(KENYAN_COUNTIES);
+        } else {
+            const filteredCounty = KENYAN_COUNTIES.filter((county) => {
+                return county.toLowerCase().includes(searchTerm.toLowerCase());
+            });
+            setFilteredCounties(filteredCounty);
+        }
+    }, [searchTerm]);
+
+    const handleCountyClick = (county) => {
+        setSelectedCounty(county);
+        setIsOpenDialog(false);
+    }
+
     return (
         <>
             <Button className="selectLocation" onClick={() => setIsOpenDialog(true)}>
                 <div className='info d-flex flex-column'>
                     <span className="label">Your Location</span>
-                    <span className="name">Nairobi</span>
+                    <span className="name">{selectedCounty !== "" ? selectedCounty : "Select county"}</span>
                 </div>
                 <span>
                     <FaAngleDown className="ms-auto" />
                 </span>
             </Button>
-            <Dialog open={isOpenDialog} onClose={() => setIsOpenDialog(false)} TransitionComponent={Transition} className="countyLocationDialog">
+            <Dialog
+                open={isOpenDialog}
+                onClose={() => setIsOpenDialog(false)}
+                TransitionComponent={Transition}
+                className="countyLocationDialog"
+            >
                 <h5>Choose your delivery location</h5>
                 <p>Enter your county and we will make arrangements...</p>
                 <Button className="close_" onClick={() => setIsOpenDialog(false)}><IoClose /></Button>
                 <div className='headerSearch w-100' >
-                    <input type='text' placeholder='Search your area' />
+                    <input
+                        type='text'
+                        placeholder='Search your area'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
                     <Button>
                         <IoIosSearch />
                     </Button>
                 </div>
                 <div>
                     <ul className="countyList mt-3">
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Nairobi</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Mombasa</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Nakuru</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Kiambu</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Machakos</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Nyeri</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Meru</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Kajiado</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Kericho</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Uasin Gishu</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Nyandarua</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Bomet</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Narok</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Laikipia</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Embu</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Tharaka Nithi</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Isiolo</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>Samburu</Button></li>
-                        <li onClick={() => setIsOpenDialog(false)}><Button>West Pokot</Button></li>
+                        {filteredCounties.map((county, index) => (
+                            <li key={index}>
+                                <Button onClick={() => handleCountyClick(county)}>{county}</Button>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </Dialog>
