33ab7d07eed46e1789774b59f4cc8b7330471667
HassanMunene <awanzihassan@gmail.com>
2025-04-26 20:45:22 +0300

    did the first sample of authentication Router just to test the login and register endpoints

---
+++ backend/routes/AuthenticationRoutes.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { loginController, registerController } from '../controllers/AuthenticationController.js';
+import rateLimit from 'express-rate-limit';
+// import { verifyToken } from '../middleware/verifyToken.js';
+// import { verifyAdmin } from '../middleware/verifyAdmin.js';
+// import { verifyUser } from '../middleware/verifyUser.js';
+
+const router = express.Router();
+
+// First of all we need to rate limit the authentication endpoints
+// This is to prevent brute force attacks on the login endpoint
+// one Ip address can only make 20 requests in 15 minutes
+
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 20,
+    message: 'Too many requests from this IP, please try again later'
+});
+
+
+// POST /api/auth/register
+router.post('/register', validateRegister, registerController);
+
+// POST /api/auth/login
+router.post('/login', authLimiter, validateLogin, loginController);
+
+export default router;
