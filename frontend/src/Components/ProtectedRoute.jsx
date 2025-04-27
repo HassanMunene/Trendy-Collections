f1c1e02819f1aad53714963d9bc1cd119545c4f2
HassanMunene <awanzihassan@gmail.com>
2025-04-27 13:13:07 +0300

    we have done the protected route to protect the admin section

---
+++ frontend/src/Components/ProtectedRoute.jsx
@@ -0,0 +1,30 @@
+import { Navigate } from "react-router-dom";
+
+import { useAuth } from "../context/AuthContext";
+import LoadingScreen from "./LoadingScreen";
+
+const ProtectedRoute = ({ children }) => {
+    const { isAuthenticated, loading } = useAuth();
+
+    if (loading) {
+        return (
+            <>
+                <LoadingScreen />
+            </>
+        )
+    }
+
+    if (!isAuthenticated) {
+        return (
+            <Navigate to="/auth/login" replace />
+        )
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default ProtectedRoute;
