c993a48afda0682c33f07dceac29aa6057e0a472
HassanMunene <awanzihassan@gmail.com>
2025-05-01 21:06:59 +0300

    we have added a route mapper to change password

--- backend/routes/AuthenticationRoutes.js
+++ backend/routes/AuthenticationRoutes.js
@@ -1,19 +1,17 @@
 import express from 'express';
 import {
     loginController,
-    registerController
+    registerController,
+    changePassword
 } from '../controllers/AuthenticationController.js';
 import rateLimit from 'express-rate-limit';
-// import { verifyToken } from '../middleware/verifyToken.js';
-// import { verifyAdmin } from '../middleware/verifyAdmin.js';
-// import { verifyUser } from '../middleware/verifyUser.js';
+import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 // First of all we need to rate limit the authentication endpoints
 // This is to prevent brute force attacks on the login endpoint
 // one Ip address can only make 20 requests in 15 minutes
-
 const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 20,
@@ -26,5 +24,8 @@ router.post('/register', registerController);
 
 // POST /api/auth/login
 router.post('/login', authLimiter, loginController);
+
+// PUT /api/auth/change-password
+router.put('/change-password', verifyToken, changePassword);
 
 export default router;
