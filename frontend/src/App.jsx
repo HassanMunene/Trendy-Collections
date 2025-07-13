7cf3a6055e6f171d85d4471742fcb67b10b6074e
HassanMunene <awanzihassan@gmail.com>
2025-07-13 13:23:32 +0300

    feat: Add CartProvider to manage cart state and update component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
 import { MobileProvider } from './context/MobileContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import LoadingScreen from './components/LoadingScreen';
@@ -12,7 +13,7 @@ import MainLayout from './layouts/MainLayout';
 // Main Layout pages
 const HomePage = lazy(() => import('./pages/customer/HomePage/index'));
 const ProductsPage = lazy(() => import('./pages/customer/productsPage/ProductsPage'))
-const ProductDetailPage = lazy(() => import('./pages/customer/ProductDetailPage'));
+const ProductDetailPage = lazy(() => import('./pages/customer/productsPage/ProductDetailPage'));
 const CartPage = lazy(() => import('./pages/customer/CartPage'));
 
 // Login and Register pages
@@ -33,37 +34,39 @@ function App() {
 	return (
 		<BrowserRouter>
 			<AuthProvider>
-				<MobileProvider>
-					{/* Suspense is used to show a fallback loading while the component is being loaded */}
-					<Suspense fallback={<LoadingScreen />}>
-						{/* This is the main layouts page */}
-						<Routes>
-							<Route path="/" element={<MainLayout />}>
-								<Route index element={<HomePage />} />
-								<Route path="products" element={<ProductsPage />} />
-								<Route path="products/:productId" element={<ProductDetailPage />} />
-								<Route path="/cart" element={<CartPage />} />
-							</Route>
-
-							{/* Authentication Routes */}
-							<Route path='/login' element={<LoginPage />} />
-							<Route path="/register" element={<RegistePage />} />
-
-							{/* the next one is the admin layout page */}
-							<Route element={<ProtectedRoute />}>
-								<Route path='/admin' element={<AdminLayout />}>
-									<Route index element={<DashboardPage />} />
-									<Route path='profile' element={<ProfilePage />} />
-									<Route path="messages" element={<MessagesPage />} />
-									<Route path='orders' element={<OrdersPage />} />
-									<Route path='products' element={<AdminProductsPage />} />
-									<Route path='customers' element={<CustomersPage />} />
-									<Route path="categories" element={<CategoriesPage />} />
+				<CartProvider>
+					<MobileProvider>
+						{/* Suspense is used to show a fallback loading while the component is being loaded */}
+						<Suspense fallback={<LoadingScreen />}>
+							{/* This is the main layouts page */}
+							<Routes>
+								<Route path="/" element={<MainLayout />}>
+									<Route index element={<HomePage />} />
+									<Route path="products" element={<ProductsPage />} />
+									<Route path="products/:productId" element={<ProductDetailPage />} />
+									<Route path="/cart" element={<CartPage />} />
 								</Route>
-							</Route>
-						</Routes>
-					</Suspense>
-				</MobileProvider>
+
+								{/* Authentication Routes */}
+								<Route path='/login' element={<LoginPage />} />
+								<Route path="/register" element={<RegistePage />} />
+
+								{/* the next one is the admin layout page */}
+								<Route element={<ProtectedRoute />}>
+									<Route path='/admin' element={<AdminLayout />}>
+										<Route index element={<DashboardPage />} />
+										<Route path='profile' element={<ProfilePage />} />
+										<Route path="messages" element={<MessagesPage />} />
+										<Route path='orders' element={<OrdersPage />} />
+										<Route path='products' element={<AdminProductsPage />} />
+										<Route path='customers' element={<CustomersPage />} />
+										<Route path="categories" element={<CategoriesPage />} />
+									</Route>
+								</Route>
+							</Routes>
+						</Suspense>
+					</MobileProvider>
+				</CartProvider>
 			</AuthProvider>
 		</BrowserRouter>
 	)
